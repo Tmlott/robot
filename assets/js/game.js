46cc818d44024c795bec0acf8e5afd8119fd4560
msoht <msoht@yahoo.com>
2022-03-24 11:06:02 -0400

    add files

---
+++ assets/js/game.js
@@ -0,0 +1,31 @@
+var playerName = window.prompt("What is your robot's name?");
+// What is this?
+var name = "your name";
+console.log(name);
+
+console.log(playerName);
+
+
+var playerName = "Bubba the Robot";
+
+//Bubba the Robot is ready for battle!
+console.log("Bubba the Robot" + " is ready for battle!");
+
+// "Bubba the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Bubba the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
